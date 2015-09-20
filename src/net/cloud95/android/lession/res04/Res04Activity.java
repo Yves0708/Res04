3dfcc013d8c8587e46b02a676472b6c158859621
Yves0708 <yvesloveisis@gmail.com>
2015-09-20 11:41:59 +0800

    Res041.0

---
+++ src/net/cloud95/android/lession/res04/Res04Activity.java
@@ -0,0 +1,14 @@
+package net.cloud95.android.lession.res04;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Res04Activity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_res04);
+    }
+
+}
